ac552f757440a9a995437c5ba5abb5b1e84cdd87
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-01-26 18:37:48 +0530

    Connected to Mongo atlus

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 # Backend Project with javascript
+
+- [Model-Link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
---
+++ src/index.js
@@ -0,0 +1,27 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+/*
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Err", error);
+      throw error;
+    })
+    app.listen(process.env.PORT, () => {
+      console.log(`App is Listening on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.log("Error: ", error);
+    throw error;
+  }
+})();
+*/
