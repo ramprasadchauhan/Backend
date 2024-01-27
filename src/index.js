b5d17f48b6c9614277dea0a86ed535e7ecc10747
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-01-27 12:38:17 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
-const app = express();
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
---
+++ src/routes/user.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/user.controller.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const router = Router();
+
+router.route("/register").post(registerUser);
+
+export default router;
+
+// import { Router } from "express";
+// import { registerUser } from "../controllers/user.controller.js";
+// import { asyncHandler } from "../utils/asyncHandler.js";
+
+// const router = Router();
+
+// router.route("/register").post(asyncHandler(registerUser));
+
+// export default router;
